e371299d441f5d1674c3f256f1158a59643b3a57
Gary Lloyd <gary.lloyd@advancedcomputersoftware.com>
2016-12-01 13:17:39 +0000

    #103456 Tidy up :checkered_flag:

--- WeatherForecast/WeatherForecast/Query/WeatherForecast.cs
+++ WeatherForecast/WeatherForecast/Query/WeatherForecast.cs
@@ -5,29 +5,35 @@ namespace WeatherForecast.Query
 {
     public class WeatherForecast : IWeatherForecast
     {
-        private readonly ISevenDayTemperatures _sevenDayTempsQuery;
-        private readonly IDaylightReport _daylightReportTempsQuery;
-        public WeatherForecast(ISevenDayTemperatures sevenDayTempsQuery, IDaylightReport daylightReportTempsQuery)
+        private readonly ISevenDayTemperatures _sevenDayTemperatureQuery;
+        private readonly IDaylightReport _daylightReportQuery;
+        public WeatherForecast(ISevenDayTemperatures sevenDayTemperatureQuery, IDaylightReport daylightReportQuery)
         {
-            _sevenDayTempsQuery = sevenDayTempsQuery;
-            _daylightReportTempsQuery = daylightReportTempsQuery;
+            _sevenDayTemperatureQuery = sevenDayTemperatureQuery;
+            _daylightReportQuery = daylightReportQuery;
         }
 
         public SevenDayForecast GetSevenDayForecast()
         {
-            var sevenDayTemperatures = _sevenDayTempsQuery.GetSevenDayTemperatures();
-            var daylightReport = _daylightReportTempsQuery.GetDaylightReport();
-            var overallDaylightReportForecast = "Mostly " + (daylightReport.Count(e => e == "Sunny") > daylightReport.Count(e => e == "Cloudy") ? "Sunny" : "Cloudy");
+            var sevenDayTemperatures = _sevenDayTemperatureQuery.GetSevenDayTemperatures();
+            var daylightReport = _daylightReportQuery.GetDaylightReport();
 
             var sevenDayForecast = new SevenDayForecast
             {
                 Temperatures = sevenDayTemperatures,
                 AverageTemperature = sevenDayTemperatures.Sum() / 7,
                 DaylightReport = daylightReport,
-                OverallDaylightReport = overallDaylightReportForecast
+                OverallDaylightReport = GetOverallDaylightForecast(daylightReport)
             };
 
             return sevenDayForecast;
+        }
+
+        private static string GetOverallDaylightForecast(string[] daylightReport)
+        {
+            return daylightReport.Count(e => e == "Sunny") > daylightReport.Count(e => e == "Cloudy")
+                       ? "Mostly Sunny"
+                       : "Mostly Cloudy";
         }
     }
 }
